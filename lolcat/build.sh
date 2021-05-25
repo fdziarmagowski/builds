6d0d4b8a55d048b9d870c4e4d213dc857181321c
Fryderyk Dziarmagowski <freddy@freddix.org>
2021-05-25 22:42:32 +0200

    add lolcat

---
+++ lolcat/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -eux
+
+repository_url="https://github.com/jaseg/lolcat.git"
+
+export DEBIAN_FRONTEND="noninteractive"
+apt update
+apt dist-upgrade -y
+apt install -y --no-install-recommends \
+  build-essential \
+  ca-certificates \
+  git \
+
+rm -rf /build/dist-inst
+
+if ! git ls-remote /build/repository -q >/dev/null 2>&1; then
+  git clone $repository_url /build/repository
+  git -C /build/repository submodule update --init --recursive
+else
+  git --work-tree=/build/repository --git-dir=/build/repository/.git checkout .
+  git --work-tree=/build/repository --git-dir=/build/repository/.git pull
+  git -C /build/repository submodule update --init --recursive
+fi
+
+cd /build/repository || exit
+
+make -j$(getconf _NPROCESSORS_ONLN)
